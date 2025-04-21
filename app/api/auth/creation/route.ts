4b51a22c0d1632143afeaf43f2a5f0cafcca984c
RaydenDarkus <svpatil20000@gmail.com>
2025-04-21 01:34:07 -0400

    latest additions

---
+++ app/api/auth/creation/route.ts
@@ -0,0 +1,42 @@
+import { prisma } from "@/app/lib/prisma";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+export const runtime = 'nodejs'; 
+
+export async function GET() {
+  try {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+    console.log(user);
+
+    if (!user?.id) {
+      return NextResponse.json({ error: "User data missing or invalid." }, { status: 400 });
+    }
+
+    // Try finding the user in the DB
+    let dbUser = await prisma.user.findUnique({ where: { id: user.id } });
+
+    if (!dbUser) {
+      // If user does not exist, create a new one
+      try {
+        dbUser = await prisma.user.create({
+          data: {
+            id: user.id,
+            firstName: user.given_name ?? "",
+            lastName: user.family_name ?? "",
+            email: user.email ?? "",
+            profileImage: user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+          },
+        });
+      } catch (error) {
+        console.error("Error creating user in database:", error);
+        return NextResponse.json({ error: "Failed to create user." }, { status: 500 });
+      }
+    }
+    return NextResponse.redirect("http://localhost:3000/");
+  } catch (error) {
+    console.error("API Error:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,64 @@
+import { ReactNode } from "react";
+import DashboardNavigation from "../components/dashboard/DashboardNavigation";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { CircleUser, MenuIcon } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
+import { redirect } from "next/navigation";
+
+const DashboardLayout = async ({ children }: { children: ReactNode }) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user.email !== "svpatil20000@gmail.com") {
+    return redirect("/");
+  }
+  return (
+    <div className="flex w-full flex-col max-w-8xl mx-auto px-4 sm:px-6 lg:px-10">
+      <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
+        <nav className="hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <DashboardNavigation />
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="shrink-0 md:hidden" variant="outline" size="icon">
+              <MenuIcon className="h-5 w-5" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <nav className="flex flex-col gap-6 text-lg font-medium mt-5">
+              <DashboardNavigation />
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <CircleUser className="w-5 h-5"></CircleUser>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <LogoutLink>Logout</LogoutLink>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </header>
+      <main className="my-5">{children}</main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
