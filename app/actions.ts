69156a4c90bf45f8f02a4e642e7de684bc4e250d
RaydenDarkus <svpatil20000@gmail.com>
2025-05-16 21:08:06 -0400

    Add Product Create

---
+++ app/actions.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import {parseWithZod} from "@conform-to/zod";
+import { productSchema } from "./lib/zodSchemas";
+
+export async function createProduct(previousState: unknown, formData: FormData) {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if(!user || user.email !== 'svpatil20000@gmail.com') {
+        return redirect("/");
+    }
+
+    const submission = parseWithZod(formData, {
+        schema: productSchema, 
+    });
+
+    if(submission.status !== "success") {
+        return submission.reply();
+    }
+}
--- app/dashboard/products/create/page.tsx
+++ app/dashboard/products/create/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { createProduct } from "@/app/actions";
 import { UploadDropzone } from "@/app/lib/uploadthing";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
@@ -8,12 +9,34 @@ import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, XIcon } from "lucide-react";
 import Link from "next/link";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { productSchema } from "@/app/lib/zodSchemas";
+import { useActionState, useState } from "react";
+import Image from "next/image";
 
 const ProductCreateRoute = () => {
+  const [images, setImages] = useState<string[]>([]);
+  const [lastResult, action] = useActionState(createProduct, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: productSchema });
+    },
+
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
+
+  const handleDelete = (index: number) => {
+    setImages(images.filter((_, i) => i !== index));
+  };
+
   return (
-    <form>
+    <form id={form.id} onSubmit={form.onSubmit} action={action}>
       <div className="flex items-center gap-4">
         <Button variant="outline" size="icon" asChild>
           <Link href="/dashboard/products">
@@ -31,23 +54,49 @@ const ProductCreateRoute = () => {
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-3">
               <Label>Name</Label>
-              <Input type="text" className="w-full" placeholder="Product Name" />
+              <Input
+                type="text"
+                className="w-full"
+                key={fields.name.key}
+                name={fields.name.name}
+                defaultValue={fields.name.initialValue}
+                placeholder="Product Name"
+              />
+              <p className="text-red-500">{fields.name.errors}</p>
             </div>
             <div className="flex flex-col gap-3">
               <Label>Description</Label>
-              <Textarea placeholder="Write your description right here..." />
+              <Textarea
+                key={fields.description.key}
+                name={fields.description.name}
+                defaultValue={fields.description.initialValue}
+                placeholder="Write your description right here..."
+              />
+              <p className="text-red-500">{fields.description.errors}</p>
             </div>
             <div className="flex flex-col gap-3">
               <Label>Price</Label>
-              <Input type="number" placeholder="$55" />
+              <Input
+                type="number"
+                key={fields.price.key}
+                name={fields.price.name}
+                defaultValue={fields.price.initialValue}
+                placeholder="$55"
+              />
+              <p className="text-red-500">{fields.price.errors}</p>
             </div>
             <div className="flex flex-col gap-3">
               <Label>Featured Product</Label>
-              <Switch />
+              <Switch
+                key={fields.isFeatured.key}
+                name={fields.isFeatured.name}
+                defaultValue={fields.isFeatured.initialValue}
+              />
+              <p className="text-red-500">{fields.isFeatured.errors}</p>
             </div>
             <div className="flex flex-col gap-3">
               <Label>Status</Label>
-              <Select>
+              <Select key={fields.status.key} name={fields.status.name} defaultValue={fields.status.initialValue}>
                 <SelectTrigger>
                   <SelectValue placeholder="Select Status" />
                 </SelectTrigger>
@@ -57,18 +106,43 @@ const ProductCreateRoute = () => {
                   <SelectItem value="archived">Archived</SelectItem>
                 </SelectContent>
               </Select>
+              <p className="text-red-500">{fields.status.errors}</p>
             </div>
             <div className="flex flex-col gap-3">
               <Label>Images</Label>
-              <UploadDropzone
-                endpoint="imageUploader"
-                onClientUploadComplete={(res) => {
-                  alert("Finished Uploading");
-                }}
-                onUploadError={() => {
-                  alert("Something went wrong.");
-                }}
-              />
+              {images.length > 0 ? (
+                <div className="flex gap-5">
+                  {images.map((image, index) => (
+                    <div key={index} className="relative w-[100px] h-[100px]">
+                      <Image
+                        height={100}
+                        width={100}
+                        src={image}
+                        alt="Product Image"
+                        className="w-full h-full object-cover rounded-lg border"
+                      />
+                      <button
+                        onClick={() => handleDelete(index)}
+                        type="button"
+                        title="close-icon"
+                        className="absolute -top-3 -right-3 bg-red-500 p-2 rounded-lg text-white"
+                      >
+                        <XIcon className="w-3 h-3" />
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <UploadDropzone
+                  endpoint="imageUploader"
+                  onClientUploadComplete={(res) => {
+                    setImages(res.map((r) => r.url));
+                  }}
+                  onUploadError={() => {
+                    alert("Something went wrong.");
+                  }}
+                />
+              )}
             </div>
           </div>
         </CardContent>
