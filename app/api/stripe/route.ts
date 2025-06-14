f250ee3928a43519dc68a909a18a8d9feb9f0bbe
RaydenDarkus <svpatil20000@gmail.com>
2025-06-13 22:26:46 -0400

    completed

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -5,7 +5,8 @@ import { headers } from "next/headers";
 
 export async function POST(req: Request) {
   const body = await req.text();
-  const signature = headers().get("Stripe-Signature") as string;
+  const headersList = await headers(); 
+  const signature = headersList.get("Stripe-Signature") as string;
   let event;
   try {
     event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_SECRET_WEBHOOK as string);
