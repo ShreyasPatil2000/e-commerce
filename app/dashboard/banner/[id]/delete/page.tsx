0f96d2d61437ad4db2e972ba2e5ae318348ee8ce
RaydenDarkus <svpatil20000@gmail.com>
2025-06-13 22:03:43 -0400

    completed

--- app/dashboard/banner/[id]/delete/page.tsx
+++ app/dashboard/banner/[id]/delete/page.tsx
@@ -4,15 +4,15 @@ import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 
-const DeleteBannerRoutes = ({ params }: { params: { id: string } }) => {
+const DeleteBannerRoutes = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
   return (
     <div className="h-[80vh] w-full flex items-center justify-center">
       <Card className="max-w-xl">
         <CardHeader>
           <CardTitle>Are you sure?</CardTitle>
           <CardDescription>
-            This action cannot be undone. This will permanantly delete this banner and remove all data from our
-            servers.
+            This action cannot be undone. This will permanantly delete this banner and remove all data from our servers.
           </CardDescription>
         </CardHeader>
         <CardFooter className="w-full flex justify-between">
@@ -20,7 +20,7 @@ const DeleteBannerRoutes = ({ params }: { params: { id: string } }) => {
             <Link href={`/dashboard/banner`}>Cancel</Link>
           </Button>
           <form action={deleteBanner}>
-            <input type="hidden" name="bannerId" value={params.id} />
+            <input type="hidden" name="bannerId" value={id} />
             <SubmitButton variant="destructive" text="Delete Banner" />
           </form>
         </CardFooter>
